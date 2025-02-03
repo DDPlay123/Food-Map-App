20078e5918b5d7a3721cc1bf81e3158dd475b0ac
DDPlay123 <a70325000@gmail.com>
2025-02-04 01:26:15 +0800

    init project

--- app/src/main/java/com/side/project/foodmap/util/gestureViewsSetting/SettingsController.kt
+++
@@ -1,7 +0,0 @@
-package com.side.project.foodmap.util.gestureViewsSetting
-
-import com.alexvasilkov.gestures.views.interfaces.GestureView
-
-interface SettingsController {
-    fun apply(view: GestureView)
-}
